b14a24159d8b137ca6265af5421ac4c57ae9d978
Kai Keiyama <s24c1049ex@s.chibakoudai.jp>
2025-10-30 13:32:44 +0900

    Add a bash

--- test.bash
+++ test.bash
@@ -1,14 +1,16 @@
-#!/bin/bash
+#!/bin/bash -xv
 #SPDX-FileCopyrightText: 2025 KaiKeiyama
 # SPDX-License-Identifier: BSD-3-Clause
-ng() {
-      echo ${1}行目が違うよ
-      res=1
+
+ng ( ) {
+       echo ${1}行目が違うよ
+　　　 res=1
 }
 
 res=0
-a=慶山
-[ "$a" = 慶山 ] || ng"$LINENO"
-[ "$a" = 山 ]   || ng"$LINENO"
 
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng "$LINENO"
+
+[ "${res}" = 0 ] && echo OK
 exit $res
