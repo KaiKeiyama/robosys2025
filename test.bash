3d8a1d1c4cd56b3c7965b3fd14ef84e8a30df3b8
Kai Keiyama <s24c1049ex@s.chibakoudai.jp>
2025-12-10 11:14:17 +0900

    add test

--- test.bash
+++ test.bash
@@ -3,40 +3,27 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 ng ( ) {
-       echo ${1}行目が違うよ
-　　　 res=1
+        echo ${1}行目が違うよ
+	res=1
 }
 
 res=0
 
-out=$(echo -n test | ./word)
-[ "${out}" =  4 24] || ng "$LINENO"
+#全体の数>選ぶ数の時
+out=$(echo -e "5\n2" | ./probabirity)
+ans="20
+10"
+[ "${out}" =  "${ans}" ] || ng "$LINENO"
+
+[ "${res}" = 0 ] && echo OK
+
+#全体の数<選ぶ数の時
+out=$(echo -e "2\n5" | ./probabirity)
+ans="0
+0"
+[ "${out}" =  "${ans}" ] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo OK
 
 
-out=$(echo -n あ| ./word)
-[ "${out}" = 1 1 ] || ng "$LINENO"
-
-[ "${res}" = 0 ] && echo OK
-
-
-out=$(echo -n 1 | ./word)
-[ "$?" = 1 ]   ng "$LINENO"
-[ "${out}" = 1 1 ] || ng "$LINENO"
-
-[ "$res" = 0 ] && echo OK
-
-
-out=$(echo -n  | ./word)
-[ "${out}" =  0 1 ] || ng "$LINENO"
-
-[ "${res}" = 0 ] && echo OK
-
-
-
-out=$(echo -n $tes | ./word)
-[ "${out}" =  4 24] || ng "$LINENO"
-
-[ "${res}" = 0 ] && echo OK
 exit $res
